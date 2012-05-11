f4fd98fb71658234204e3658ddbb35798fdf3a6d
pete gamache <pete@where.com>
2012-05-11 14:44:20 -0400

    uppercased macros

--- arduinbros.ino
+++ arduinbros.ino
@@ -12,7 +12,7 @@ byte received_byte;
 
 // dicks
 char *dicks[16] = {
-  "8D",              // idx = 0, len = 2.  CHOAD.
+  "8D",               // idx = 0, len = 2.  CHOAD.
   "8=D",
   "8==D",
   "8===D",
@@ -29,15 +29,15 @@ char *dicks[16] = {
   "8==============D",
   "8===============D" // idx = 15, len = 17. SWANGIN.
 };
-#define dickfor(N) dicks[(N) & 0xF]
+#define DICKFOR(N) dicks[(N) & 0xF]
 
 // here we define our sequence of rainbow colors
 byte rainbow_r[7] = {0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x4B, 0xCC};
 byte rainbow_g[7] = {0x00, 0x80, 0xFF, 0xFF, 0x00, 0x00, 0x22};
 byte rainbow_b[7] = {0x00, 0x00, 0x00, 0x00, 0xFF, 0x82, 0x88};
 int _rainbow = 0; // starting offset from RED for first char of a line
-// rainbow_rgb returns the r, g, b bytes for the rainbow color N=0..6
-#define rainbow_rgb(N) rainbow_r[(N)], rainbow_g[(N)], rainbow_b[(N)]
+// RAINBOW_RGB returns the r, g, b bytes for the rainbow color N=0..6
+#define RAINBOW_RGB(N) rainbow_r[(N)], rainbow_g[(N)], rainbow_b[(N)]
 
 // GOLDELOX text stuff
 #define FONT 0     // 5x7
@@ -45,17 +45,17 @@ int _rainbow = 0; // starting offset from RED for first char of a line
 #define NCOLS 16
 #define BGCOLOR 0,0,0
 byte _row=0, _col=0;   // current text cursor position
-#define advance_rainbow  if (_rainbow==6) _rainbow=0; else _rainbow++;
-#define newline _row++; _col=0; advance_rainbow;
-#define origin  _row=0; _col=0; advance_rainbow;
+#define ADVANCE_RAINBOW  if (_rainbow==6) _rainbow=0; else _rainbow++;
+#define NEWLINE _row++; _col=0; ADVANCE_RAINBOW;
+#define ORIGIN  _row=0; _col=0; ADVANCE_RAINBOW;
 
 // macros to pack byte R, G, B data into two bytes, rg and gb
-#define rg(R,G,B) (((R) & 0xf8) | ((G) >> 5))
-#define gb(R,G,B) (((G) << 5) | ((B) >> 3))
+#define RG(R,G,B) (((R) & 0xf8) | ((G) >> 5))
+#define GB(R,G,B) (((G) << 5) | ((B) >> 3))
 
 // this macro handles the ack byte returned by the GOLDELOX.
 // returns 1 on success (ACK), 0 on error (NAK)
-#define handle_ack \
+#define HANDLE_ACK \
         while (! Serial.available()) ; return Serial.read()==0x06 ? 1 : 0;
 
 
@@ -64,31 +64,31 @@ byte _row=0, _col=0;   // current text cursor position
 int drawChar(char c, byte r, byte g, byte b) {
   Serial.write(0x54);
   Serial.write(c);
-  if (_col >= NCOLS) { newline; }
+  if (_col >= NCOLS) { NEWLINE; }
   Serial.write(_col++);
   Serial.write(_row);
-  Serial.write(rg(r,g,b));
-  Serial.write(gb(r,g,b));
-  handle_ack;
+  Serial.write(RG(r,g,b));
+  Serial.write(GB(r,g,b));
+  HANDLE_ACK;
 }
 
-// drawString prints an ASCII string at the current position, with newline.
+// drawString prints an ASCII string at the current position, with NEWLINE.
 // Color is in a rainbow fashion, a la lolcat.
 void drawString(char *str) {
   int color;
   color = _rainbow;
 
   while (*str) {
-    drawChar(*str++, rainbow_rgb(color));
+    drawChar(*str++, RAINBOW_RGB(color));
     if (color == 6) color = 0; else color++;
   }
-  newline;
+  NEWLINE;
 }
 
 // draw a screenful of dicks
 void drawDicks() {
   for (int row=0; row < NROWS; row++) {
-    drawString(dickfor(random(0,15))); 
+    drawString(DICKFOR(random(0,15))); 
   }
 }
 
@@ -96,22 +96,22 @@ void drawDicks() {
 int setFont(byte f) {
   Serial.write(0x46);
   Serial.write(f);
-  handle_ack;
+  HANDLE_ACK;
 }
 
 // changes screen background color
 int setBg(int r, int g, int b) {
   Serial.write(0x42);
-  Serial.write(rg(r,g,b));
-  Serial.write(gb(r,g,b));
-  handle_ack;
+  Serial.write(RG(r,g,b));
+  Serial.write(GB(r,g,b));
+  HANDLE_ACK;
 }
 
-// clears the screen and moves text cursor to origin
+// clears the screen and moves text cursor to ORIGIN
 int clearScreen() {
-  origin;
+  ORIGIN;
   Serial.write(0x45);
-  handle_ack;
+  HANDLE_ACK;
 }
 
 
@@ -143,7 +143,7 @@ void setup() {
 
 void loop() {
   drawDicks();
-  delay(2500);
+  delay(1500);
   clearScreen();
 }
 
